011ab760a332d522680b24554656db1a763ba69b
=Bruno Igor <=brigor7@gmail.com>
2021-01-26 14:52:10 -0300

    Implementando signout em Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+import AuthContext from '../../context/Auth';
 
-const Dashboard: React.FC = () => <View />;
+const Dashboard: React.FC = () => {
+  const { signed, signOut } = useContext(AuthContext);
+  function handleSignout() {
+    signOut();
+  }
+  return (
+    <View style={styles.container}>
+      <Button title="SignOut" onPress={handleSignout} />
+    </View>
+  );
+};
 
 export default Dashboard;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
