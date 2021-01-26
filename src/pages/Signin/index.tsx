9f42d070ee3f180ce680beaca4548ce40d47607a
=Bruno Igor <=brigor7@gmail.com>
2021-01-26 12:32:07 -0300

    Estilizando button Signin

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, StyleSheet } from 'react-native';
 
 const Signin: React.FC = () => (
-  <View>
+  <View style={styles.container}>
     <Button title="Signin" onPress={() => {}} />
   </View>
 );
 
 export default Signin;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
