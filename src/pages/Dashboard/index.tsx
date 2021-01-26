23b977d88ae23c82fe817ce72cf7d9597ec26311
=Bruno Igor <=brigor7@gmail.com>
2021-01-26 16:36:23 -0300

    Incluindo o nome do usuario no dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,16 @@
 import React, { useContext } from 'react';
-import { View, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import AuthContext from '../../context/Auth';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useContext(AuthContext);
+  const { signOut, user } = useContext(AuthContext);
   function handleSignout() {
     signOut();
   }
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Bem vindo</Text>
+      <Text style={styles.bold}>{user?.name}</Text>
       <Button title="Sign out" onPress={handleSignout} />
     </View>
   );
@@ -21,5 +23,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+  },
+  bold: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 50,
   },
 });
