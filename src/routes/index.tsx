b679e7e5aa0944495cd37268b98e8c4ba23040b4
=Bruno Igor <=brigor7@gmail.com>
2021-01-26 14:37:11 -0300

    Fazendo a autenticação e chamando dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../context/Auth';
 
 import AuthRoutes from './auth.routes';
+import AppRoutes from './app.routes';
 
 const Routes: React.FC = () => {
-  return <AuthRoutes />;
+  const { signed } = useContext(AuthContext);
+  console.log(signed);
+  return signed ? <AppRoutes /> : <AuthRoutes />;
 };
 
 export default Routes;
