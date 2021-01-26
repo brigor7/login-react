7932e51ba3af87265f8c5f598eccfa961f5bf5d1
=Bruno Igor <=brigor7@gmail.com>
2021-01-26 12:47:25 -0300

    Capturate "remote" data from service/auth

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { View, Button, StyleSheet } from 'react-native';
+import { signIn } from '../../service/auth';
 
-const Signin: React.FC = () => (
-  <View style={styles.container}>
-    <Button title="Signin" onPress={() => {}} />
-  </View>
-);
+const Signin: React.FC = () => {
+  async function handleSignIn() {
+    const response = await signIn();
+    console.log(response);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title="Signin" onPress={handleSignIn} />
+    </View>
+  );
+};
 
 export default Signin;
 
